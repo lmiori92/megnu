344b4122cb2415969e5f44a289127200d9b09253
Lorenzo Miori <lorenzo.miori@gmail.com>
2016-09-11 11:30:33 +0200

    - menu files ready for subtree push

---
+++ menu.c
@@ -0,0 +1,340 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Lorenzo Miori (C) 2015 [ 3M4|L: memoryS60<at>gmail.com ]
+
+    Version History
+        * 1.0 initial
+
+*/
+
+/**
+ * @file menu.c
+ * @author Lorenzo Miori
+ * @date Jun 2016
+ * @brief MENU: handling of simple menu logic for character displays
+ */
+
+
+#include "display.h"    /* display primitives */
+#include "menu.h"       /* menu definitions */
+#include "lib.h"        /* utilities */
+
+static char* BOOL_LABELS[2] = { "NO", "YES" };
+static uint8_t BOOL_VALUES[2] = { (uint8_t)false, (uint8_t)true };
+
+/** Static declarations **/
+static void menu_extra_display(void *extra, e_item_type type);
+static void menu_index_edit(t_menu_state *state, uint8_t count, bool increment);
+static void menu_extra_edit(t_menu_item *item, bool increment);
+
+/* State */
+static t_menu_state *g_state;
+static t_menu_item  *g_item;
+static uint8_t       g_count;
+static uint8_t       g_page;
+static t_menu_cb     g_menu_event_cb;
+
+void menu_init_bool_list(t_menu_extra_list *extra)
+{
+    extra->count = 2U;
+    extra->labels = BOOL_LABELS;
+    extra->values = BOOL_VALUES;
+}
+
+void menu_init(t_menu_state *state)
+{
+    if (g_state != NULL)
+    {
+        state->state = MENU_NOT_SELECTED;
+        state->index = 0U;
+        state->prev = 1U;
+        state->diff = 1U;
+    }
+}
+
+void menu_event_callback(t_menu_cb menu_cb)
+{
+    g_menu_event_cb = menu_cb;
+}
+
+static void menu_extra_display(void *extra, e_item_type type)
+{
+
+    uint8_t u8_tmp;
+    uint16_t u16_tmp;
+    uint32_t u32_tmp;
+    t_menu_extra_list *ext_ptr_tmp;
+
+    if (extra != NULL)
+    {
+        /* dereference and operate the pointer based on the defined type */
+        switch(type)
+        {
+            case MENU_TYPE_LIST:
+                /* 8-bit array index */
+                /* display the "enum-like" element */
+                ext_ptr_tmp = (t_menu_extra_list*)extra;
+                u8_tmp = ext_ptr_tmp->ptr;
+                display_write_string(ext_ptr_tmp->labels[u8_tmp]);
+                break;
+            case MENU_TYPE_NUMERIC_8:
+                /* 8-bit number */
+                u8_tmp = (*((uint8_t*)extra));
+                display_write_number((uint16_t)u8_tmp, false);
+                break;
+            case MENU_TYPE_NUMERIC_16:
+                /* 16-bit number */
+                u16_tmp = (*((uint16_t*)extra));
+                display_write_number(u16_tmp, false);
+                break;
+            case MENU_TYPE_NUMERIC_32:
+                /* 16-bit number */
+                u32_tmp = (*((uint32_t*)extra));
+                display_write_number(u32_tmp, false);
+                break;
+            default:
+                /* not implemented */
+                break;
+        }
+    }
+}
+
+static void menu_index_edit(t_menu_state *state, uint8_t count, bool increment)
+{
+    if (count > 0)
+    {
+        if (increment == true)
+        {
+            /* increment - "menu down" */
+            if (state->index < (count - 1U))
+            {
+                state->prev = state->index;
+                state->index++;                    /* increment value */
+            }
+        }
+        else
+        {
+            /* decrement - "menu up" */
+            if (state->index > 0U)
+            {
+                state->prev = state->index;
+                state->index--;                    /* increment value */
+            }
+        }
+    }
+}
+
+static void menu_extra_edit(t_menu_item *item, bool increment)
+{
+    t_menu_extra_list* tmp_extra;
+    uint16_t tmp;
+
+    if (item->extra != NULL)
+    {
+        /* dereference and operate the pointer based on the defined type */
+        switch(item->type)
+        {
+            case MENU_TYPE_LIST:
+                /* 8-bit number for indexing the type "list" */
+                tmp_extra = (t_menu_extra_list*)item->extra;
+                if (increment == true)
+                {
+                    if (tmp_extra->ptr < (tmp_extra->count - 1))
+                        tmp_extra->ptr++;
+                }
+                else
+                {
+                    if (tmp_extra->ptr > 0U)
+                        tmp_extra->ptr--;
+                }
+                break;
+            case MENU_TYPE_NUMERIC_8:
+                /* 8-bit number */
+                tmp = (uint16_t)(*((uint8_t*)item->extra));
+                if (increment == true) lib_sum(&tmp, UINT8_MAX, g_state->diff);
+                else lib_diff(&tmp, g_state->diff);
+                *((uint8_t*)item->extra) = (uint8_t)tmp;
+                break;
+            case MENU_TYPE_NUMERIC_16:
+                /* 16-bit number */
+                tmp = (*((uint16_t*)item->extra));
+                if (increment == true) lib_sum(&tmp, UINT16_MAX, g_state->diff);
+                else lib_diff(&tmp, g_state->diff);
+                *((uint16_t*)item->extra) = tmp;
+                break;
+            default:
+                /* not implemented */
+                break;
+        }
+    }
+}
+
+/**
+This routine displays the current state of the menu i.e. labels, selection
+and possibly extra data like ON-OFF labels or numeric values.
+*/
+void menu_display(void)
+{
+
+    uint8_t id1;
+    uint8_t id2;
+
+    if ((g_state == NULL ) || (g_item == NULL))
+    {
+    	/* No menu page is selected */
+    	return;
+    }
+
+    display_clean();
+
+    /* selected item arrow display */
+    if (g_state->index > g_state->prev)
+    {
+        /* "down" */
+        display_set_cursor(1, 0);
+    }
+    else
+    {
+        /* "up" */
+        display_set_cursor(0, 0);
+    }
+
+    display_write_char(0x7EU);
+
+    id1 = g_state->prev > g_state->index ? g_state->index : g_state->prev;
+    id2 = g_state->prev > g_state->index ? g_state->prev : g_state->index;
+
+    /* display labels and extra data */
+    /* for faster execution time, work with the pointer of the items */
+
+    display_set_cursor(0U, 1U);                                       /* cursor right after the (possible) arrow */
+    display_write_string((g_item + id1)->label);         /* label */
+    display_advance_cursor(1U);
+
+    /* [ or space */
+    display_write_char(((id1 == g_state->index) && (g_state->state == MENU_SELECTED)) ? '[' : ' ');
+    menu_extra_display((g_item + id1)->extra, (g_item + id1)->type);
+    /* ] or space */
+    display_write_char(((id1 == g_state->index) && (g_state->state == MENU_SELECTED)) ? ']' : ' ');
+
+    display_set_cursor(1U, 1U);                                       /* cursor right after the (possible) arrow */
+    display_write_string((g_item + id2)->label);    /* label */
+    display_advance_cursor(1U);
+
+    /* [ or space */
+    display_write_char(((id2 == g_state->index) && (g_state->state == MENU_SELECTED)) ? '[' : ' ');
+    menu_extra_display((g_item + id2)->extra, (g_item + id2)->type);
+    /* ] or space */
+    display_write_char(((id2 == g_state->index) && (g_state->state == MENU_SELECTED)) ? ']' : ' ');
+
+}
+
+e_menu_output_event menu_event(e_menu_input_event event)
+{
+
+    t_menu_item *item;
+    e_menu_output_event output_event = MENU_EVENT_OUTPUT_NONE;
+
+    if ((g_state == NULL ) || (g_item == NULL))
+    {
+    	/* No menu page is selected */
+    }
+    else
+    {
+
+        item = g_item + g_state->index;
+
+        switch(event)
+        {
+            case MENU_EVENT_NONE:
+                /* no event; NOOP */
+                break;
+            case MENU_EVENT_CLICK:
+                if (item->extra != NULL)
+                {
+                    /* complex entry (editable) -> toggle selected / unselected */
+                    g_state->state = (g_state->state == MENU_SELECTED) ? MENU_NOT_SELECTED : MENU_SELECTED;
+                    output_event = (g_state->state == MENU_SELECTED) ? MENU_EVENT_OUTPUT_SELECT : MENU_EVENT_OUTPUT_DESELECT;
+                }
+                else if (item->type == MENU_TYPE_BACK)
+                {
+                    output_event = MENU_EVENT_OUTPUT_BACK;
+                }
+                else
+                {
+                    /* if a simple entry, do nothing (probably changing menu anyhow) */
+                    g_state->state = MENU_NOT_SELECTED;
+                    output_event = MENU_EVENT_OUTPUT_CLICK;
+                }
+                break;
+            case MENU_EVENT_CLICK_LONG:
+                /* NOOP for the moment */
+                output_event = MENU_EVENT_OUTPUT_CLICK_LONG;
+                break;
+            case MENU_EVENT_LEFT:
+                if (g_state->state == MENU_SELECTED)
+                {
+                    menu_extra_edit(item, false);
+                    output_event = MENU_EVENT_OUTPUT_EXTRA_EDIT;
+                }
+                else
+                {
+                    menu_index_edit(g_state, g_count, false);
+                    output_event = MENU_EVENT_OUTPUT_INDEX_EDIT;
+                }
+                break;
+            case MENU_EVENT_RIGHT:
+                if (g_state->state == MENU_SELECTED)
+                {
+                    menu_extra_edit(item, true);
+                    output_event = MENU_EVENT_OUTPUT_EXTRA_EDIT;
+                }
+                else
+                {
+                    menu_index_edit(g_state, g_count, true);
+                    output_event = MENU_EVENT_OUTPUT_INDEX_EDIT;
+                }
+                break;
+            default:
+                /* no event; NOOP */
+                break;
+        }
+
+        /* Callback for the external world (if any) */
+        if ((output_event != MENU_EVENT_OUTPUT_NONE) && (g_menu_event_cb != NULL))
+        {
+            g_menu_event_cb(output_event, g_state->index, g_page);
+        }
+    }
+
+    return output_event;
+}
+
+void menu_set(t_menu_state *state, t_menu_item *item, uint8_t count, uint8_t page)
+{
+    g_state = state;
+    g_item  = item;
+    g_count = count;
+    g_page  = page;
+
+    /* init to a known state */
+    menu_init(state);
+}
+
+void menu_set_diff(uint16_t diff)
+{
+    if (g_state != NULL)
+        g_state->diff = (diff == 0U) ? 1U : diff;
+}
---
+++ menu.h
@@ -0,0 +1,114 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Lorenzo Miori (C) 2015 [ 3M4|L: memoryS60<at>gmail.com ]
+
+    Version History
+        * 1.0 initial
+
+*/
+
+/**
+ * @file menu.h
+ * @author Lorenzo Miori
+ * @date Apr 2016
+ * @brief MENU: handling of simple menu logic for character displays
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#include <stdint.h>
+
+/** Enumeration of supported events the menu system can handle (input) */
+typedef enum
+{
+    MENU_EVENT_NONE,
+    MENU_EVENT_CLICK,
+    MENU_EVENT_CLICK_LONG,
+    MENU_EVENT_LEFT,
+    MENU_EVENT_RIGHT
+} e_menu_input_event;
+
+/** Enumeration of supported events the menu system can generate (output) */
+typedef enum _e_menu_output_event
+{
+    MENU_EVENT_OUTPUT_NONE,
+    MENU_EVENT_OUTPUT_CLICK,
+    MENU_EVENT_OUTPUT_CLICK_LONG,
+    MENU_EVENT_OUTPUT_INDEX_EDIT,   /**< item index value has been changed */
+    MENU_EVENT_OUTPUT_EXTRA_EDIT,   /**< item extra value has been changed */
+    MENU_EVENT_OUTPUT_SELECT,       /**< item is now selected */
+    MENU_EVENT_OUTPUT_DESELECT,     /**< item is not selected anymore */
+    MENU_EVENT_OUTPUT_BACK     /**< "back" item has been clicked */
+} e_menu_output_event;
+
+/** Enumeration of supported menu item types */
+typedef enum
+{
+    MENU_TYPE_NONE,            /**< Label only */
+    MENU_TYPE_LIST,            /**< Menu item extra type is a list */
+    MENU_TYPE_NUMERIC_8,       /**<  */
+    MENU_TYPE_NUMERIC_16,      /**<  */
+    MENU_TYPE_NUMERIC_32,      /**<  */
+    MENU_TYPE_BACK             /**< Menu item to get to the previous page */
+} e_item_type;
+
+/** Enumeration of the possible states a menu item can be */
+typedef enum
+{
+    MENU_NOT_SELECTED,         /**< Menu item is not selected to perform actions on it */
+    MENU_SELECTED              /**< Menu item is selected and actions can be performed on it */
+} e_menu_item_state;
+
+/** Structure for the extra LIST item information (labels and values) */
+typedef struct
+{
+    uint8_t  count;           /**< Total number of labels */
+    uint8_t  ptr;             /**< The index of the currently active label */
+    char**   labels;         /**< Labels to associate the ptr value to */
+    uint8_t* values;         /**< List of values to associate the ptr to */
+} t_menu_extra_list;
+
+/** Structure for describing the menu item */
+typedef struct
+{
+    char*           label;  /**< Item has this string as label */
+    void*           extra;  /**< When != NULL an extra is appended, depending on the type of menu */
+    e_item_type     type;   /**< Menu type; determines extra field type */
+} t_menu_item;
+
+/** Structure for the current menu state */
+typedef struct
+{
+    uint8_t index;              /**< Selected menu item */
+    uint8_t prev;               /**< Selected menu item */
+    e_menu_item_state   state;  /**< Item state */
+    uint16_t diff;              /**< Determines scrolling "speed" / "velocity" (e.g. from an encoder or repeated keypress) */
+} t_menu_state;
+
+/** Event callback function */
+typedef void (*t_menu_cb)(e_menu_output_event event, uint8_t index, uint8_t page);
+
+/**  EXTERNAL LINKAGE FUNCTIONS **/
+
+void menu_init(t_menu_state *state);
+void menu_init_bool_list(t_menu_extra_list *extra);
+void menu_event_callback(t_menu_cb menu_cb);
+void menu_set(t_menu_state *state, t_menu_item *item, uint8_t count, uint8_t page);
+void menu_set_diff(uint16_t diff);
+void menu_display(void);
+e_menu_output_event menu_event(e_menu_input_event event);
+
+#endif /* MENU_H_ */
